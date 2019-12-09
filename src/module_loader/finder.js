59a98ba1a28206898da85c9c68fb1063fd7a5df8
NathanPB <nathan.pbombana@gmail.com>
2019-12-09 18:35:56 -0300

    Added code documentation to the entire module system

--- src/module_loader/finder.js
+++ src/module_loader/finder.js
@@ -3,15 +3,39 @@ const fs = require('fs');
 const ModuleType = require('.').ModuleType;
 const MODULES_PATH = require('.').MODULES_PATH;
 
+/**
+ * Discovers all the possible modules in a certain directory.
+ * 
+ * A "possible module" is (un)shorthand for directory.
+ * 
+ * @see [fs.readdirSync]
+ * @param modulesDir {string} - The directory to search. Default is [MODULES_PATH].
+ * @returns {Promise<string[]>} - The list with the absolute path of all the possible modules.
+ */
 const discoverModulesInDirectory = async (modulesDir = MODULES_PATH) => {
   return fs.readdirSync(modulesDir).map(it => path.resolve(modulesDir, it));
 };
 
+/**
+ * Reads a module directory and collects data about the module.
+ * 
+ * The directory must have a package.json file in its root with
+ * the entries "main", "auditum.type" and "auditum.name".
+ * The package.json file must be readable.
+ * 
+ * It will throw exceptions if the conditions above are not satisfied.
+ * 
+ * @param moduleDir {string} - The directory to search for the module data.
+ * @returns {ModulePackageInfo} - The module data.
+ * @throws {Error} - If the directory does not contain a readable json-formatted "package.json" file.
+ * @throws {Error} - If the found "package.json" file does not contain a "main" entry.
+ * @throws {Error} - If the found "package.json" file does not contain a "auditum" entry.
+ * @throws {Error} - If the found "package.json" file does not contain a "auditum.name" entry.
+ * @throws {Error} - If the found "package.json" file does not contain a "auditum.type" entry.
+ */
 const readModuleInfo = (moduleDir) => {
   const packageContent = require(path.resolve(moduleDir, 'package.json'));
-  const auditumData = packageContent.auditum;
-  const mainScript = packageContent.main;
-
+  const { auditum: auditumData, main: mainScript } = packageContent;
   if(!auditumData) {
     throw new Error('No "auditum" entry found in package.json');
   }
@@ -38,6 +62,16 @@ const readModuleInfo = (moduleDir) => {
   });
 };
 
+/**
+ * Reads a module "package.json" file and checks the access to its main file.
+ * 
+ * The file provided in the "main" entry must be readable.
+ *
+ * @see [readModuleInfo]
+ * @param moduleDir {string} - The directory to search for the information.
+ * @returns {Promise<ModulePackageInfo>} - The object with the package information.
+ * @throws {Error} - If the file provided in the "main" entry of the "package.json" file does not exist or is not readable.
+ */
 const loadModulePackage = async (moduleDir) => {
   const modulePackage =  readModuleInfo(moduleDir);
   
@@ -45,6 +79,17 @@ const loadModulePackage = async (moduleDir) => {
   return modulePackage;
 };
 
+/**
+ * Discover all the modules in a specified directory and validate them.
+ * 
+ * It also logs all the found modules. Being them found or not.
+ * 
+ * @see discoverModulesInDirectory
+ * @see readModuleInfo
+ * @see loadModulePackage
+ * @param modulesDir {string} - The directory to search for the modules. Default to [MODULES_PATH].
+ * @returns {Promise<[ModulePackageInfo]>} - Information about all the valid discovered packages.
+ */
 const discoverModules = async (modulesDir = MODULES_PATH) => {
   const logLoad = async (dir) => {
     try {
--- src/module_loader/index.js
+++ src/module_loader/index.js
@@ -1,11 +1,82 @@
 const path = require('path');
 
+/**
+ * @typedef RequiredModuleObject
+ *
+ * The actual exported structure of an Auditum NodeJS module's main script.
+ *
+ * @type {function | {onRequest: function, onResponse, function}}
+ */
+
+/**
+ * @typedef ModulePackageInfo
+ * @type {{name: string, type: string, main: string}}
+ *
+ * @property name {string} - The name of the module.
+ * @property type {string} - The type of the module.
+ * @property main {string} - The absolute path to the start script of the module.
+ */
+
+/**
+ * @typedef ModuleObject
+ *
+ * The complete structure of an Auditum module.
+ *
+ * @type {{ ...ModulePackageInfo, module: RequiredModuleObject }}
+ */
+
+/**
+ * The available module types.
+ *
+ * @type {{SCRAP: string, IO: string}}
+ */
 const ModuleType = {
   IO: 'io',
   SCRAP: 'scraper'
 };
 
+/**
+ * The default directory where the modules are located.
+ *
+ * It has the value of the environmental variable "AUDITUM_MODULES".
+ * If that environmental variable is not found, then the
+ * directory used is "modules" relative to the pwd
+ *
+ * ```${pwd}/modules```
+ *
+ * E.g.: ``/home/guest/auditum/modules``
+ * Where "/home/guest/auditum" is the pwd
+ *
+ * @type {string}
+ */
 const MODULES_PATH = process.env.AUDITUM_MODULES || path.resolve('modules');
+
+/**
+ * Checks the structure of a module. The input object
+ *
+ * To be satisfied, those conditions must be satisfied:
+ * - Case of Scraper module:
+ *   - It must export a function named "onRequest";
+ *   - It must export a function named "onResponse".
+ *
+ * - Case of IO module:
+ *   - It must be a function.
+ *
+ * @param moduleInfo {ModuleObject} - The module to inspect
+ * @returns {boolean} - True if the module is valid, false otherwise.
+ * @throws {Error} - If the input parameter is not satisfied.
+ * @throws {Error} - If the module type is unknown.
+ */
+const checkStructure = (moduleInfo) => {
+  switch (moduleInfo.type) {
+    case ModuleType.IO: return (
+      typeof moduleInfo.module.onRequest === 'function' &&
+      typeof moduleInfo.module.onResponse === 'function'
+    );
+    case ModuleType.SCRAP: return typeof moduleInfo.module === 'function';
+    default: throw new Error(`Unknown module type: ${moduleInfo.type}`);
+  }
+};
 
 module.exports = {
   ModuleType, MODULES_PATH
